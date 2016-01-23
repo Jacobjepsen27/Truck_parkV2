1540a93cd9a4dc3558753f5480255f4613071629
Jacobjepsen27 <Jacob.worck@gmail.com>
2016-01-23 19:26:56 +0100

    Initial commit

---
+++ app/src/main/java/jepsen/dk/truck_park/MainActivity.java
@@ -0,0 +1,29 @@
+package jepsen.dk.truck_park;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.gmap);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btn){
+            Intent gm = new Intent(this, MapsActivity.class);
+            startActivity(gm);
+        }
+    }
+}
